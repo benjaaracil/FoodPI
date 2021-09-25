35cb8408f7b195fa655ab17721622c6e7153c4fa
benjaaracil <benjaminaracil@gmail.com>
2021-09-25 16:34:07 -0300

    Npm´s

--- api/src/db.js
+++ api/src/db.js
@@ -6,8 +6,6 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, APP_API_KEY
 } = process.env;
 
-// const YOUR_API_KEY = process.env.APP_API_KEY;
-
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
