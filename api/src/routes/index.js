e6ef4e80a74eee0eb58405d5d8654d1c15705c98
benjaaracil <benjaminaracil@gmail.com>
2021-09-25 16:43:07 -0300

    Index Routes Config Done

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,17 @@
-const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
+const RouteDiets = require ("./components/Diets")
+const RouteRecipes = require ("./components/Recipes")
 
 
-const router = Router();
+
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use("/Diets", RouteDiets)
+router.use("/Recipes", RouteRecipes)
 
 
 module.exports = router;
