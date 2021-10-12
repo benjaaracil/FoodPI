90fab71fae5842442789383f97caf88931fd7b82
benjaaracil <benjaminaracil@gmail.com>
2021-10-12 14:54:48 -0300

    Without testing

--- client/src/components/CreateRecipe/Create.jsx
+++ client/src/components/CreateRecipe/Create.jsx
@@ -4,17 +4,6 @@ import { postRecipes } from "../../actions";
 import NavBar from "../NavBar/NavBar";
 import "./Create.css"
 
-export function validate(input) {   // 7- ESTAS SON LAS VALIDACIONES "EN VIVO", PARA VER SI LO QUE ESCRIBE EL USUARIO ESTA YENDO BIEN O NO
-    let errors = {};
-    if (!input.title) {
-     errors.title ='Title is required'; //el usuario tiene que escribir algo como minimo
-    }
-    if(!input.summary){
-       errors.summary = "Summary is required" //el usuario tiene que escribir algo como minimo
-    }
-    return errors;
-};
-
 export default function Create(){
 const dietas = useSelector(state => state.diets);
 
@@ -31,6 +20,17 @@ const [errors, setErrors] = React.useState({
     title: "",
     summary: ""
 })
+
+function validate(input) {   // 7- ESTAS SON LAS VALIDACIONES "EN VIVO", PARA VER SI LO QUE ESCRIBE EL USUARIO ESTA YENDO BIEN O NO
+    let errors = {};
+    if (!input.title) {
+     errors.title ='Title is required'; //el usuario tiene que escribir algo como minimo
+    }
+    if(!input.summary){
+       errors.summary = "Summary is required" //el usuario tiene que escribir algo como minimo
+    }
+    return errors;
+};
 
 function handleSubmit(e){
     e.preventDefault();
@@ -78,31 +78,30 @@ function handleChangeCheck(e){
                     <input value = {input.title} type = "text" name = "title" onChange={(e) => handleChange(e)} ></input>
                 </div>
                     <p className = "Error">{errors.title? errors.title :null}</p>
-            <div className ="DivChiquito">
-                <label className = "Titulo">Summary</label>
-                <input className = {errors.summary} value = {input.summary} type = "text" name = "summary" onChange={(e) => handleChange(e)}></input>
-            </div>
-                <p className = "Error">{errors.summary? errors.summary :null}</p>
-            <div className ="DivChiquito">    
-                <label className = "Titulo">Score</label>
-                <input type = "number" min = "0" max = "100" name = "spoonacularScore" onChange={(e) => handleChange(e)}></input>
-            </div>
-            <div className ="DivChiquito">
-                <label className = "Titulo">Health Score</label>
-                <input type = "number" min = "0" max = "100" name = "healthScore" onChange={(e) => handleChange(e)}></input>
-            </div>
-            <div className ="DivChiquito">
-                <label className = "Titulo">Instructions</label>
-                <input type = "text" name = "analyzedInstructions" onChange={(e) => handleChange(e)}></input>
-            </div>
+                <div className ="DivChiquito">
+                    <label className = "Titulo">Summary</label>
+                    <input className = {errors.summary} value = {input.summary} type = "text" name = "summary" onChange={(e) => handleChange(e)}></input>
+                </div>
+                    <p className = "Error">{errors.summary? errors.summary :null}</p>
+                <div className ="DivChiquito">    
+                    <label className = "Titulo">Score</label>
+                    <input type = "number" min = "0" max = "100" name = "spoonacularScore" onChange={(e) => handleChange(e)}></input>
+                </div>
+                <div className ="DivChiquito">
+                    <label className = "Titulo">Health Score</label>
+                    <input type = "number" min = "0" max = "100" name = "healthScore" onChange={(e) => handleChange(e)}></input>
+                </div>
+                <div className ="DivChiquito">
+                    <label className = "Titulo">Instructions</label>
+                    <input type = "text" name = "analyzedInstructions" onChange={(e) => handleChange(e)}></input>
+                </div>
             </div>
             {/* Checkbox dietas */}
             <div className = "Checking">
-                {/* {console.log("Dietas", dietas)} */}
                 {dietas.map((d,index) => 
                     <div key = {index}>
-                        <label>{d.name}</label>
                         <input className = "CheckingBox"type='checkbox' onChange={(e) => handleChangeCheck(e)} name={d.name} id = {d.id}/>
+                        <label>{d.name}</label>
                     </div>
                 )}
             </div>
